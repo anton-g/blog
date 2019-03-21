f33f656c5cb9d5e4135e3ed253d087cc5d2086c4
Anton Gunnarsson <gson.anton@hotmail.com>
2019-03-21 19:31:31 +0100

    Update bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,15 +3,18 @@ import React from 'react'
 class Bio extends React.Component {
   render() {
     return (
-      <div style={{
-        marginBottom: '3rem',
-        display: 'flex',
-        alignItems: 'center'
-      }}>
+      <div
+        style={{
+          marginBottom: '3rem',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
         <p style={{ margin: 0, paddingLeft: '1em' }}>
-          Written by <strong>Anton Gunnarsson</strong>. He lives in Stockholm and works with stuff on the internet.{' '}
+          Written by me, <strong>Anton Gunnarsson</strong>. I live in Stockholm
+          and work at <a href="https://mpyadigital.com">Mpya Digital</a>.{' '}
           <a href="https://twitter.com/awnton">
-            You should follow him on Twitter.
+            Reach out with any questions on twitter.
           </a>
         </p>
       </div>
