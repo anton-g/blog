131745db336dd9993fa26083071c2486d0804642
Anton Gunnarsson <gson.anton@hotmail.com>
2020-02-07 22:28:30 +0100

    Add header anim

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,39 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { useSpring, animated, config } from 'react-spring'
+import { useHover } from 'react-use-gesture'
 
-class Project extends React.Component {
-  render() {
-    const title = (
-      <Link
-        to={'/'}
-        style={{
-          textDecoration: 'none',
-        }}
-      >
-        anton gunnarsson
-      </Link>
-    )
+export default function Header({ small }) {
+  const title = (
+    <Link
+      to={'/'}
+      style={{
+        textDecoration: 'none',
+      }}
+    >
+      {'anton gunnarsson'.split('').map((l, i) => (
+        <Letter key={i}>{l}</Letter>
+      ))}
+    </Link>
+  )
 
-    return (
-      <header>{this.props.small ? <h3>{title}</h3> : <h1>{title}</h1>}</header>
-    )
-  }
+  return <header>{small ? <h3>{title}</h3> : <h1>{title}</h1>}</header>
 }
 
-export default Project
+const Letter = ({ children }) => {
+  const c = {
+    ...config.wobbly,
+    tension: 430,
+  }
+
+  const [{ y }, set] = useSpring(() => ({ y: 0, config: c }))
+  const bind = useHover(({ hovering }) => set({ y: hovering ? -20 : 0 }))
+
+  return (
+    <div {...bind()} style={{ display: 'inline-block' }}>
+      <animated.div style={{ y, display: 'inline-block', whiteSpace: 'pre' }}>
+        {children}
+      </animated.div>
+    </div>
+  )
+}
