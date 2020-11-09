8a7c2383d224341a04cf4221263e015ea411d8ec
Anton Gunnarsson <gson.anton@hotmail.com>
2020-11-09 21:16:45 +0100

    Update colors

--- src/constants.js
+++ src/constants.js
@@ -5,19 +5,19 @@ const primaryColor = tinycolor('hsl(173, 70%, 53%)')
 export const COLORS = {
   background: {
     light: 'hsl(0, 100%, 99%)',
-    dark: 'hsl(0, 0%, 5%)',
+    dark: 'hsl(0, 0%, 5%)'
   },
   text: {
     light: 'hsl(0, 0%, 5%)',
-    dark: 'hsl(0, 100%, 99%)',
+    dark: 'hsl(0, 100%, 99%)'
   },
   heading: {
     light: 'hsl(157, 15%, 15%)',
-    dark: 'hsl(157, 5%, 90%)',
+    dark: 'hsl(157, 5%, 90%)'
   },
   primary: {
     light: primaryColor.toHslString(),
-    dark: primaryColor.toHslString(),
+    dark: primaryColor.toHslString()
   },
   quote: {
     light: primaryColor
@@ -26,13 +26,13 @@ export const COLORS = {
       .toHslString(),
     dark: primaryColor
       .clone()
-      .darken(40)
-      .toHslString(),
+      .darken(45)
+      .toHslString()
   },
   code: {
     light: 'hsl(0, 10%, 90%)',
-    dark: 'hsla(0, 0%, 20%, 50%)',
-  },
+    dark: 'hsla(0, 0%, 20%, 50%)'
+  }
 }
 
 export const SOUND_MODE_KEY = 'sound-mode'
